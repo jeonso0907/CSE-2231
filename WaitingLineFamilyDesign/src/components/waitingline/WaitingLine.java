dddd5a2f0fc84edf29ce9d4b9949bdaf2f500a20
LeafyLeaves <lim.1252@osu.edu>
2020-11-17 11:34:06 -0500

    A

---
+++ WaitingLineFamilyDesign/src/components/waitingline/WaitingLine.java
@@ -0,0 +1,63 @@
+package components.waitingline;
+
+/**
+ * {@code WaitingLineKernel} enhanced with secondary methods.
+ *
+ * @param <T>
+ *            type of {@code WaitingLine} entries
+ * @mathdefinitions <pre>
+ * IS_TOTAL_PREORDER (
+ *   r: binary relation on T
+ *  ) : boolean is
+ *  for all x, y, z: T
+ *   ((r(x, y) or r(y, x))  and
+ *    (if (r(x, y) and r(y, z)) then r(x, z)))
+ *
+ * IS_SORTED (
+ *   s: string of T,
+ *   r: binary relation on T
+ *  ) : boolean is
+ *  for all x, y: T where (<x, y> is substring of s) (r(x, y))
+ * </pre>
+ */
+public interface WaitingLine<T> extends WaitingLineKernel<T> {
+
+    /**
+     * Reports the front of {@code this}.
+     *
+     * @return the front entry of {@code this}
+     * @aliases reference returned by {@code front}
+     * @requires this /= <>
+     * @ensures <front> is prefix of this
+     */
+    T front();
+
+    /**
+     * Replaces the front of {@code this} with {@code x}, and returns the old
+     * front.
+     *
+     * @param x
+     *            the new front entry
+     * @return the old front entry
+     * @aliases reference {@code x}
+     * @updates this
+     * @requires this /= <>
+     * @ensures <pre>
+     * <replaceFront> is prefix of #this  and
+     * this = <x> * #this[1, |#this|)
+     * </pre>
+     */
+    T replaceFrontCustomer(T x);
+
+    /**
+     * Concatenates ("appends") {@code q} to the end of {@code this}.
+     *
+     * @param q
+     *            the {@code Queue} to be appended to the end of {@code this}
+     * @updates this
+     * @clears q
+     * @ensures this = #this * #q
+     */
+    void combineCustomer(WaitingLine<T> q);
+
+}
