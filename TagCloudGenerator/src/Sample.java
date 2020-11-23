b1bf93c64510bd0cc00533b99b319f47998fa4e8
LeafyLeaves <lim.1252@osu.edu>
2020-11-23 13:41:04 -0500

    Update Sample.java
    Comments and contracts

--- TagCloudGenerator/src/Sample.java
+++ TagCloudGenerator/src/Sample.java
@@ -26,6 +26,11 @@ public final class Sample {
         // no code needed here
     }
 
+    /**
+     * Comparator<Map.Pair<String, Integer>> implementation to be used to
+     * compare the key. Compare {@code Map.Pair<String, Integer>}s in
+     * lexicographic order.
+     */
     private static class wordComparator
             implements Comparator<Map.Pair<String, Integer>> {
         @Override
@@ -36,6 +41,11 @@ public final class Sample {
         }
     }
 
+    /**
+     * Comparator<Map.Pair<String, Integer>> implementation to be used to
+     * compare the value. Compare {@code Map.Pair<String, Integer>}s in
+     * lexicographic order.
+     */
     private static class countComparator
             implements Comparator<Map.Pair<String, Integer>> {
         @Override
@@ -45,6 +55,15 @@ public final class Sample {
         }
     }
 
+    /**
+     * Makes {@code Queue} that has all words without the separators
+     *
+     * @param String
+     *            the input file
+     * @return Queue
+     * @requires inputFile != null
+     * @ensures
+     */
     public static Queue<String> wordsInput(String inputFile) {
 
         // Initialize a queue to store only words
@@ -70,16 +89,21 @@ public final class Sample {
             // Check each character in the line
             for (int i = 0; i < charLine.length; i++) {
 
+                // Get a character from charLine
                 char character = charLine[i];
 
+                // If the result is -1, add characters to form a word then add it to the new Queue
                 if (specialChar.indexOf(character) == -1) {
                     if (i == charLine.length - 1) {
                         word += character;
                         words.enqueue(word);
-                    } else {
+                    }
+                    // If not, add characters to form a word
+                    else {
                         word += character;
                     }
 
+                    // If length of Queue that has words without a special character is more than 0, add it to the new Queue
                 } else if (word.length() > 0) {
                     words.enqueue(word);
                     word = "";
@@ -91,82 +115,156 @@ public final class Sample {
 
         in.close();
 
+        // Return the new Queue without special characters
         return words;
     }
 
+    /**
+     * Makes {@code Map} that counts every word from Queue
+     *
+     * @param String
+     *            the input file
+     * @return Queue
+     * @requires inputFile != null
+     * @ensures
+     */
     public static Map<String, Integer> wordsCount(Queue<String> words) {
 
+        // Initialize the new Map
         Map<String, Integer> wordsMap = new Map1L<>();
 
+        // Check if the length of words is more than 0
         while (words.length() > 0) {
 
+            // Remove a word from Queue
             String word = words.dequeue();
 
+            // Make every character in a word into a lower case
             word = word.toLowerCase();
 
+            // If a word is not in the Map, add a word into the Map
             if (!wordsMap.hasKey(word)) {
                 wordsMap.add(word, 1);
-            } else {
+            }
+            // If a word is already in the Map, just add the value by one
+            else {
                 int currentCount = wordsMap.value(word);
                 wordsMap.replaceValue(word, currentCount + 1);
             }
 
         }
 
+        // Return the new Map
         return wordsMap;
     }
 
+    /**
+     * Makes {@code Queue} that has 100 words in lexicographic order
+     *
+     * @param String
+     *            the input file
+     * @return Queue
+     * @requires inputFile != null
+     * @ensures
+     */
     public static Queue<Integer> sortCount(Map<String, Integer> wordsMap) {
 
+        // Initialize the comparator
         Comparator<Pair<String, Integer>> countComparator = new countComparator();
 
+        // Initialize the new Map
         Map<String, Integer> sortMap = wordsMap.newInstance();
 
+        // Initialize the new Queue
         Queue<Integer> maxAndMin = new Queue1L<>();
 
+        // Initialize the new SortingMachine
         SortingMachine<Map.Pair<String, Integer>> sortMachine = new SortingMachine1L<Pair<String, Integer>>(
                 countComparator);
 
+        // Check every Pair in wordsMap
         for (Pair<String, Integer> pair : wordsMap) {
+
+            // Add Pair into SortingMachine
             sortMachine.add(pair);
         }
+
+        // Change the SortingMachine into Extraction Mode and sort in increasing order
         sortMachine.changeToExtractionMode();
 
+        // Check every Pair until 100
         for (int i = 0; i < 100; i++) {
 
+            // Remove the first Pair from SortingMachine
             Pair<String, Integer> topPair = sortMachine.removeFirst();
+
+            // Stores a key
             String topWord = topPair.key();
+
+            // Stores a value
             int topCount = topPair.value();
 
+            // Add the minimum value into the Queue
             if (i == 0) {
                 maxAndMin.enqueue(topCount);
-            } else if (i == 99) {
+            }
+            // Add the maximum value into the Queue
+            else if (i == 99) {
                 maxAndMin.enqueue(topCount);
             }
 
+            // Add the Pair into the new Map
             sortMap.add(topWord, topCount);
         }
+
+        // Transfer the new Map into the old Map
         wordsMap.transferFrom(sortMap);
 
+        // Return the Queue
         return maxAndMin;
     }
 
+    /**
+     * Makes {@code SortingMachine} that has all words in increasing order
+     *
+     * @param Map
+     *            the input file
+     * @return Queue
+     * @requires inputFile != null
+     * @ensures
+     */
     public static SortingMachine<Map.Pair<String, Integer>> sortWord(
             Map<String, Integer> wordsMap) {
 
+        // Initialize the comparator
         Comparator<Pair<String, Integer>> countComparator = new wordComparator();
 
+        // Initialize the new SortingMachine
         SortingMachine<Map.Pair<String, Integer>> sortMachine = new SortingMachine1L<Pair<String, Integer>>(
                 countComparator);
 
+        // Check every Pair in wordsMap
         for (Pair<String, Integer> pair : wordsMap) {
+
+            // Add the Pair into the SortingMachine
             sortMachine.add(pair);
         }
+
+        // Change the SortingMachine into Extraction Mode and sort
         sortMachine.changeToExtractionMode();
 
+        // Return the new SortingMachine
         return sortMachine;
     }
 
+    /**
+     * Generates the header of HTML
+     *
+     * @param out
+     *            outputs the code
+     * @param inputFile
+     *            the {@code String} that gets the input file
+     */
     public static void header(SimpleWriter out, String inputFile) {
 
         out.println("<html>");
@@ -179,17 +277,40 @@ public final class Sample {
 
     }
 
+    /**
+     * Generates the body of HTML
+     *
+     * @param out
+     *            outputs the code
+     * @param inputFile
+     *            the {@code String} that gets the input file
+     * @param maxAndMin
+     *            the {@code Queue} that gets the minimum and the maximum
+     * @param sortedWords
+     *            the {@code SortingMachine} that gets sorted 100 Pairs
+     */
     public static void body(SimpleWriter out, String inputFile,
             Queue<Integer> maxAndMin,
             SortingMachine<Map.Pair<String, Integer>> sortedWords) {
 
+        // Initialize the average
         int average = 0;
+
+        // Remove the maximum from Queue
         int max = maxAndMin.dequeue();
+
+        // Remove the minimum from Queue
         int min = maxAndMin.dequeue();
+
+        // Subtract the maximum and the minimum to find the difference
         int difference = max - min;
+
+        // Add every value into the average
         for (Map.Pair<String, Integer> word : sortedWords) {
             average += word.value();
         }
+
+        // Divide the average by the length of the SortingMachine
         average /= 100;
 
         out.println("<body data-new-gr-c-s-check-loaded=\"14.984.0\" "
@@ -199,6 +320,7 @@ public final class Sample {
         out.println("<div class=\"cdiv\">");
         out.println("<p class=\"cbox\">");
 
+        // Calculate the right font size for each Pair
         for (Map.Pair<String, Integer> wordAndCount : sortedWords) {
             String word = wordAndCount.key();
             int count = wordAndCount.value();
@@ -227,13 +349,29 @@ public final class Sample {
 
     }
 
+    /**
+     * Generates the HTML
+     *
+     * @param outputFile
+     *            the {@code String} that gets the output file
+     * @param inputFile
+     *            the {@code String} that gets the input file
+     * @param maxAndMin
+     *            the {@code Queue} that gets the minimum and the maximum
+     * @param sortedWords
+     *            the {@code SortingMachine} that gets sorted 100 Pairs
+     */
     public static void htmlGenerator(String outputFile, String inputFile,
             Queue<Integer> maxAndMin,
             SortingMachine<Map.Pair<String, Integer>> sortedWords) {
 
+        // Initialize
         SimpleWriter out = new SimpleWriter1L(outputFile);
 
+        // Calls the method "header"
         header(out, inputFile);
+
+        // Calls the method "body"
         body(out, inputFile, maxAndMin, sortedWords);
 
     }
@@ -245,22 +383,31 @@ public final class Sample {
      *            the command line arguments; unused here
      */
     public static void main(String[] args) {
+        // Initialize the output stream
         SimpleWriter out = new SimpleWriter1L();
 
         String inputFile = "data/importance.txt";
 
+        // Generate the Queue from the input file
         Queue<String> words = wordsInput(inputFile);
 
+        // Generate the Map from words
         Map<String, Integer> wordsMap = wordsCount(words);
 
+        // Generate the Queue from wordsMap
         Queue<Integer> maxAndMin = sortCount(wordsMap);
 
+        // Generate the SortingMachine from wordsMap
         SortingMachine<Map.Pair<String, Integer>> sortedWords = sortWord(
                 wordsMap);
 
+        // Generate the HTML
         htmlGenerator("test.html", "data/importance.txt", maxAndMin,
                 sortedWords);
 
+        /*
+         * Close output stream
+         */
         out.close();
     }
 
